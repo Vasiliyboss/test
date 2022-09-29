c708f4b0e8156c72ac0f9e1c86838cf0102eb0ba
Vasiliy <mazurboss2016@gmail.com>
2022-09-29 21:38:56 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,47 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+class App extends React.Component {
+  state = {
+    name: '',
+    number: '',
+  };
+  changeName = e => {
+    // console.log(e.target.value);
+    // this.setState({ name: e.target.value });
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  handlerForm = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <div>
+        <form action="" onSubmit={this.handlerForm}>
+          <label htmlFor="">
+            Name
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.changeName}
+            />
+          </label>
+          <label htmlFor="">
+            Number
+            <input
+              type="text"
+              name="number"
+              value={this.state.number}
+              onChange={this.changeName}
+            />
+          </label>
+
+          <button type="submit">add</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default App;
